c09ed26e25ac6d401a005475dcf20141743e199d
adamhalverson1 <76791907+adamhalverson1@users.noreply.github.com>
2022-09-28 09:10:58 -0500

    edit eas.json for dev
    edit eas.json for dev

--- App.js
+++ App.js
@@ -3,21 +3,19 @@ import RockPaperScissors from './components/RockPaperScissors';
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
 
 //creating Google AdMob Unit ID variable. 
-const adUnitId = 'ca-app-pub-3345304889044888/6748235043'
+const adUnitId = 'ca-app-pub-3345304889044888/6748235043';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <RockPaperScissors/>
-      <BannerAd style={styles.bannerAd}
-            unitId={TestIds.BANNER} //Test ID
-            //unitID={adUnitId} // Production Unit ID 
-            sizes={[BannerAdSize.FULL_BANNER]}
-            requestOptions={{
-            requestNonPersonalizedAdsOnly: true,
-            tagForUnderAgeOfConsent: true
-            }}
-    />
+      <BannerAd
+        unitId={adUnitId}
+        size={BannerAdSize.FULL_BANNER}
+        requestOptions={{
+        requestNonPersonalizedAdsOnly: true,
+        }}
+      />
     </View>
   );
 }
